551218f2c6d517abd560d5ee0f493dea6707eb51
John Dust <d24dust@hotmail.com>
2021-10-23 14:14:28 -0400

    add: deck_spec tests

--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Deck, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "generates 52 cards on init" do
+    d = Deck.new
+
+    expect(d.cards.size).to eq(52)
+  end
+
+  it "each card has suit and number" do
+    d = Deck.new
+    expect(d.cards.all? {|card| card[:suit] && card[:number]}).to eq(true)
+  end
 end
