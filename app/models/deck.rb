0db0c7a3d1c59fa7061e4a643ed0e19579a30bad
John Dust <d24dust@hotmail.com>
2021-10-23 14:18:39 -0400

    feat: initialize method

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,16 @@
 class Deck
   attr_reader :cards
-  
+
+  def initialize
+    @cards = []
+    suits = ["clubs", "diamonds", "hearts", "spades"]
+    numbers = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+    
+    suits.each do |suit|
+      numbers.each do |number|
+        @cards.push({suit: suit, number: number})
+      end
+    end
+  end
+
 end
