b132aa063b8338dc7390f513c88bb5e387ade8c8
John Dust <d24dust@hotmail.com>
2021-10-23 15:15:33 -0400

    add: #deal_cards_to method test

--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -17,4 +17,13 @@ RSpec.describe Deck, type: :model do
     d = Deck.new_from_json(g.deck_cards)
     expect(d.cards[0]).to eq({"suit"=>"clubs", "number"=>"ace"})
   end
+
+  it "can deal cards to player" do
+    g = Game.create
+    g.players.create
+
+    g.deck.deal_cards_to(7, g.players[0])
+    expect(g.deck.cards.size).to eq(52 - 7)
+  end
+  
 end
