ecc773108fa6bdf8efcf8da50d93085d757dacfe
John Dust <d24dust@hotmail.com>
2021-10-23 15:16:05 -0400

    add: card dealing test

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe Player, type: :model do
     g = p.create_game
     expect(p.game).to_not be_nil
   end
+
+  it "can receive cards from game deck" do
+    g = Game.create
+    p = g.players.create
+
+    g.deck.deal_cards_to(7, g.players[0])
+    expect(p.cards.size).to eq(7)
+  end
+
 end
