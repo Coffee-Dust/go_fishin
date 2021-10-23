879531e66b9a20bb0d357d30cc477aae8ab6b9de
John Dust <d24dust@hotmail.com>
2021-10-23 13:36:03 -0400

    add: test for game relation

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "belongs to a game" do
+    p = Player.create
+    g = p.create_game
+    expect(p.game).to_not be_nil
+  end
 end
