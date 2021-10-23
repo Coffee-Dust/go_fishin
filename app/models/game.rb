971d1a27ad09a0be5821e6200b63527832f81278
John Dust <d24dust@hotmail.com>
2021-10-23 13:18:34 -0400

    add: generate_room_code method

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,17 @@
 class Game < ApplicationRecord
+  before_save :generate_room_code
+
+  def generate_room_code
+    chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    code = ""
+    6.times do 
+      code += chars[rand(chars.size)]
+    end
+    if self.class.find_by(room_code: code)
+      generate_room_code
+    else
+      self.room_code = code
+    end
+  end
+  
 end
