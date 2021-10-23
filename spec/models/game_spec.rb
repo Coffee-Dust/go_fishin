b86199a13e46b59f2d29e0d04e0ce50f6a8a230b
John Dust <d24dust@hotmail.com>
2021-10-23 13:18:22 -0400

    add: tests for .room_code

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a room code auto generated" do
+    g = Game.create
+    expect(g.room_code).to_not be_nil
+  end
+
+  it "has uniq room code generated" do
+    100.times do
+      Game.create
+    end
+    g = Game.new
+    g.generate_room_code
+    expect(Game.find_by(room_code: g.room_code)).to be_nil
+  end
 end
