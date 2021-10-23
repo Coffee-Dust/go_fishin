490cae7912219d5504c69e060ef69575bbf29aee
John Dust <d24dust@hotmail.com>
2021-10-23 14:51:21 -0400

    change: generate_room_code behavior

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,10 @@
 class Game < ApplicationRecord
   has_many :players
-  before_save :generate_room_code
+  after_initialize :set_defaults, unless: :persisted?
+
+  def set_defaults
+    self.room_code = generate_room_code
+  end
 
   def generate_room_code
     chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -11,7 +15,7 @@ class Game < ApplicationRecord
     if self.class.find_by(room_code: code)
       generate_room_code
     else
-      self.room_code = code
+      return code
     end
   end
 
