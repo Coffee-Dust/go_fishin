e9ad1737c0518a08d741ea0ccc8440f62b50716e
John Dust <d24dust@hotmail.com>
2021-10-23 15:17:41 -0400

    add: setter/getter for cards attr

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,11 @@
 class Player < ApplicationRecord
   belongs_to :game
+
+  def cards=(card_array)
+    @cards = {cards: card_array}.to_json
+  end
+
+  def cards
+    @cards ? JSON.parse(@cards)["cards"] : []
+  end
 end
