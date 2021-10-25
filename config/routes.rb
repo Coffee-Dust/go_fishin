4e930b94a2d6da48f95c7484ec81f6121d6ff1f1
John Dust <d24dust@hotmail.com>
2021-10-25 12:49:37 -0400

    add: POST /games/join route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :games, only: [:index, :show, :create, :destroy]
-
+  post "/games/join", to: "games#join"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
