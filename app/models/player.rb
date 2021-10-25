725144afd8fabfdc74e5e5af7f5c481d603cdc07
John Dust <d24dust@hotmail.com>
2021-10-25 15:33:18 -0400

    fix: #cards getter/setter methods

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,10 +2,10 @@ class Player < ApplicationRecord
   belongs_to :game
 
   def cards=(card_array)
-    @cards = {cards: card_array}.to_json
+    super({cards: card_array}.to_json)
   end
 
   def cards
-    @cards ? JSON.parse(@cards)["cards"] : []
+    self[:cards] ? JSON.parse(self[:cards])["cards"] : []
   end
 end
