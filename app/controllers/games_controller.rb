de1e3bbb57f8c404b20cd408320719bb76cc0374
John Dust <d24dust@hotmail.com>
2021-10-25 13:15:19 -0400

    implement: join action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,6 +6,16 @@ class GamesController < ApplicationController
     redirect_to game_path(@game.room_code)
   end
 
+  def join
+    @game = Game.find_by(room_code: params[:room_code])
+    redirect_to games_path, alert: "Could not find room with that code!" if !@game
+
+    if !@game.players.include?(helpers.current_player)
+      p = @game.players.create
+      session["player_id"] = p.id
+    end
+    redirect_to game_path(@game.room_code)
+  end
 
   def index
 
