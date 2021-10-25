61d7f3a8b58ce0672fec590724d671adfd4c1092
John Dust <d24dust@hotmail.com>
2021-10-25 14:10:42 -0400

    add: index view tests

--- spec/system/games_system_spec.rb
+++ spec/system/games_system_spec.rb
@@ -1,4 +1,22 @@
 require 'rails_helper'
 RSpec.describe "Games", type: :system do
 
+  describe "Index View" do
+    it "VIEW index: will create new game on button click" do
+      visit "/games"
+      click_button "Create New Game"
+      g = Game.all.last
+
+      expect(page).to have_text(g.room_code)
+    end
+
+    it "VIEW index: will join game with a valid room_code" do
+      g = Game.create
+      visit "/games"
+      fill_in "room_code", with: g.room_code
+      click_button "Join"
+      expect(page).to have_text("Waiting on players")
+    end
+  end
+
 end
