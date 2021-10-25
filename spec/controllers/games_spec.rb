c6e2755a565518c452dbe450586b2e3eb8db7d39
John Dust <d24dust@hotmail.com>
2021-10-25 12:50:46 -0400

    add: tests for POST /games

--- spec/controllers/games_spec.rb
+++ spec/controllers/games_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
 
+  describe "POST /games" do
+    it "creates a new game instance" do
+      post :create, {}
+      expect(Game.all.last).to_not be_nil
+    end
+
+    it "redirects to game instance" do
+      post :create, {}
+      g = Game.all.last
+      expect(response).to redirect_to("/games/#{g.room_code}")
+    end
+
+    it "adds you as a player in the session" do
+      session[:player_id] = nil
+      post :create, {}
+      expect(session["player_id"]).to_not be_nil
+    end
+
+    it "adds you as a player in the game" do
+      session[:player_id] = nil
+      post :create, {}
+      g = Game.all.last
+      p = Player.find(session["player_id"])
+
+      expect(p.game).to eq(g)
+    end
+  end
+
 end
