f80c00c6374747541c1cbc1f702c7e261f55c6df
John Dust <d24dust@hotmail.com>
2021-10-25 15:33:53 -0400

    add: start route for games

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :games, only: [:index, :show, :create, :destroy]
+  resources :games, only: [:index, :show, :create, :destroy] do
+    get "/start", to: "games#start"
+  end
   post "/games/join", to: "games#join"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
