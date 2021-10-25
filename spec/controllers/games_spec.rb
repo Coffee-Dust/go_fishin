683de251ea5ce2471d311277d9a35372f4bdd73c
John Dust <d24dust@hotmail.com>
2021-10-25 13:14:49 -0400

    add: tests for POST /games/join

--- spec/controllers/games_spec.rb
+++ spec/controllers/games_spec.rb
@@ -30,4 +30,28 @@ RSpec.describe GamesController, type: :controller do
     end
   end
 
+  describe "POST /games/join" do
+    before do
+      session[:player_id] = nil
+      g = Game.create
+      params = {room_code: g.room_code}
+      post :join, params: {room_code: g.room_code}
+    end
+
+    it "will join game with valid code" do
+      expect(response).to have_http_status(:redirect)
+    end
+
+    it "will add player to session" do
+      expect(session["player_id"]).to_not be_nil
+    end
+
+    it "will add player to game" do
+      g = Game.all.last
+      p = Player.find(session["player_id"])
+
+      expect(p.game).to eq(g)
+    end
+  end
+
 end
