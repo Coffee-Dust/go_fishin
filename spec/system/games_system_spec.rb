44b3d3ae9db0711a15771e40e1a88a50455e5aa6
John Dust <d24dust@hotmail.com>
2021-10-25 14:12:36 -0400

    add: Gameplay system tests

--- spec/system/games_system_spec.rb
+++ spec/system/games_system_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 RSpec.describe "Games", type: :system do
 
+  describe "Gameplay system" do
+    it "Will create a new player for game on joining" do
+      g = Game.create
+      visit "/games"
+      fill_in "room_code", with: g.room_code
+      click_button "Join"
+
+      expect(g.players.size).to eq(1)
+    end
+  end
+
+
   describe "Index View" do
     it "VIEW index: will create new game on button click" do
       visit "/games"
