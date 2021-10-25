f6ae6afb7058f2c73cc91efcac19800ec0397f4e
John Dust <d24dust@hotmail.com>
2021-10-25 12:52:37 -0400

    add: create action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,11 @@
 class GamesController < ApplicationController
+  def create
+    @game = Game.create
+    p = @game.players.create
+    session["player_id"] = p.id
+    redirect_to game_path(@game.room_code)
+  end
+
 
   def index
 
