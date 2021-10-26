9cc5a496ef81e338efbb9578534b7b74442b7a03
John Dust <d24dust@hotmail.com>
2021-10-26 14:38:51 -0400

    add: #is_turn? method

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,10 @@
 class Player < ApplicationRecord
   belongs_to :game
 
+  def is_turn?
+    self.game.current_player.id == self.id
+  end
+
   def cards=(card_array)
     super({cards: card_array}.to_json)
   end
